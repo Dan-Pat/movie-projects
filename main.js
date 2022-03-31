365491b7d9d4e5eeda3ed41c6b2be2a292bda9ef
Pat Barfield <patrickhbarfield@gmail.com>
2022-03-31 12:02:34 -0500

    created first card and functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 /* Main Fetch Function */
 // function getUserLastPushEvent(username) {
@@ -13,3 +13,37 @@ fetch(url)
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
+function populateBody(dataIn) {
+    $("#pop-body").html(createMovieCards(dataIn));
+}
+
+function createMovieCards(dataIn) {
+    let html = '<div>';
+    for (let i = 0; i < 5; i++) {
+        html += cardForge(dataIn[i]);
+    }
+    html += '</div>';
+    return html;
+}
+
+// language=HTML
+function cardForge(dataIn) {
+    let html = '';
+    html += `
+        <div class="cards row col-3">
+            <h5>Card</h5>
+            <div id="p-container" class="col-4">
+
+            </div>
+            <ul class="col-5 justify-content-around">
+                <li class="list-item">Movie Name: ${dataIn.title}</li>
+                <li class="list-item">Still Image: ${dataIn.image}</li>
+                <li class="list-item">Rating: ${dataIn.imDbRating}</li>
+                <li class="list-item">Genre: ${dataIn.genres}</li>
+            </ul>
+            <div>
+            </div>
+        </div>`
+
+    return html;
+}
