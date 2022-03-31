d83e353b8ee4f526e292d6016a652c82363945cc
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-31 14:48:56 -0500

    updated index.html, main.js, style.css added card population functionality

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
 "use strict";
 
+
+const IMDB_URL = `https://imdb-api.com/API/AdvancedSearch/${IMDB_KEY}?locations=Texas`;
+const GLITCH_URL = 'https://codeup-restful-example.glitch.me/reviews';
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reviewObj),
+};
+
+/* INVOKE IMDB FETCH FUNCTION */
+imdbFetch(IMDB_URL);
+
 /* Main Fetch Function */
-// function coolFunc(dataIn) {
-//
-// }
-
-
-
-
-let url = `https://imdb-api.com/API/AdvancedSearch/${IMDB_KEY}?locations=Texas`;
-
-fetch(url)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-function populateBody(dataIn) {
-    $("#pop-body").html(createMovieCards(dataIn));
+function imdbFetch(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(response => populateBody(response))
+        .catch(err => console.error(err));
 }
 
+/* SELECT DIV FOR CARD POPULATION */
+function populateBody(dataIn) {
+    console.log(dataIn.results[0].title);
+
+    $('#pop-body').html(createMovieCards(dataIn));
+}
+
+/* ITERATE THROUGH OBJECTS FUNCTION */
 function createMovieCards(dataIn) {
+
     let html = '<div>';
+
     for (let i = 0; i < 5; i++) {
-        html += cardForge(dataIn[i]);
+        html += cardForge(dataIn.results[i]);
     }
+
     html += '</div>';
+
     return html;
 }
 
+/* CREATE CARDS FUNCTION */
 // language=HTML
 function cardForge(dataIn) {
     let html = '';
-    html += `
-        <div class="cards row col-3">
-            <h5>Card</h5>
-            <div id="p-container" class="col-4">
 
+    html += `
+        <div id="card-parent" class="cards row col-3">
+            <h5>Movie Name: ${dataIn.title}</h5>
+            <div id="p-container" class="col-4">
+                <img src="${dataIn.image}" alt="movie poster">
             </div>
             <ul class="col-5 justify-content-around">
-                <li class="list-item">Movie Name: ${dataIn.title}</li>
-                <li class="list-item">Still Image: ${dataIn.image}</li>
                 <li class="list-item">Rating: ${dataIn.imDbRating}</li>
-                <li class="list-item">Genre: ${dataIn.genres}</li>
+                <li>Genre: ${dataIn.genres}</li>
             </ul>
             <div>
             </div>
@@ -49,3 +64,9 @@ function cardForge(dataIn) {
 
     return html;
 }
+
+
+/* ADDING A MOVIE FETCH */
+fetch(GLITCH_URL, options)
+    .then(response => console.log(response)) /* review was created successfully */
+    .catch(error => console.error(error)); /* handle errors */
