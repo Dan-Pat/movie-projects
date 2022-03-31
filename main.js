a2e7d6652d400a18c496753a1f1d57e134870c5a
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-03-31 10:45:01 -0500

    updated index.html, main.js added fetch functionality

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict"
 
+/* Main Fetch Function */
+// function getUserLastPushEvent(username) {
+//
+// }
+
+
+let url = `https://imdb-api.com/API/AdvancedSearch/${IMDB_KEY}?locations=Texas`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
